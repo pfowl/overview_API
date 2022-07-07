4329ed9170b97db4412498026a4ca31809fbf1e0
Kedir Zeinu <kedirmohammedzeinu@gmail.com>
2022-07-07 17:48:42 -0400

    ignored node modules

--- server/utils/k6AllProducts.js
+++ server/utils/k6AllProducts.js
@@ -5,15 +5,22 @@ import { Counter } from 'k6/metrics';
 export const requestCounter = new Counter('http_reqs');
 
 export const options = {
-  vus: 100,
-  duration: '15s'
-}
+  stages: [
+    { duration: '10s', target: 100 }, // below normal load
+    { duration: '1m', target: 100 },
+    { duration: '10s', target: 1400 }, // spike to 1400 users
+    { duration: '3m', target: 1400 }, // stay at 1400 for 3 minutes
+    { duration: '10s', target: 100 }, // scale down. Recovery stage.
+    { duration: '3m', target: 100 },
+    { duration: '10s', target: 0 },
+  ],
+};
 
-const randomCount = () =>Math.floor(Math.random() * (100000 - 99900) + 99900);
-
+const randomCount = () => Math.floor(Math.random() * (100000 - 99900) + 99900);
 export default function () {
-  const url = `http://localhost:3001/products/${randomCount()}/styles`;
+  const url = `http://localhost:3000/products/${randomCount()}/styles`;
   const res = http.get(url);
+  sleep(1)
   check(res, {
     'is status 200': r => r.status === 200,
     'transaction time < 200ms': r => r.timings.duration < 200,
